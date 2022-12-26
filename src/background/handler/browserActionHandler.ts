78a1d9ac506c64db0ebaa6bf0707aa9c8ec67ec3
Sriram Hariharan <sghsri@gmail.com>
2022-12-26 17:29:58 -0600

    implemented the browser action

--- src/background/handler/browserActionHandler.ts
+++ src/background/handler/browserActionHandler.ts
@@ -2,8 +2,19 @@ import BrowserActionMessages from 'src/shared/messages/BrowserActionMessages';
 import { MessageHandler } from 'src/shared/types';
 
 const browserActionHandler: MessageHandler<BrowserActionMessages> = {
-    disableBrowserAction(context) {},
-    enableBrowserAction(context) {},
+    disableBrowserAction({ sender, sendResponse }) {
+        // by setting the popup to an empty string, clicking the browser action will not open the popup.html.
+        // we can then add an onClickListener to it from the content script
+        chrome.action.setPopup({ tabId: sender.tab?.id, popup: '' }).then(sendResponse);
+    },
+    enableBrowserAction({ sender, sendResponse }) {
+        chrome.action
+            .setPopup({
+                tabId: sender.tab?.id,
+                popup: 'popup.html',
+            })
+            .then(sendResponse);
+    },
 };
 
 export default browserActionHandler;
--- src/shared/messages/BrowserActionMessages.ts
+++ src/shared/messages/BrowserActionMessages.ts
@@ -1,7 +1,8 @@
 import { MessageDefinition } from '../types';
 
 export default interface BrowserActionMessages extends MessageDefinition {
-    /** Enable the browser action */
+    /** make it so that clicking the browser action will open the popup.html */
     enableBrowserAction: () => void;
+    /** make it so that clicking the browser action will respond to interactions from the content script */
     disableBrowserAction: () => void;
 }
