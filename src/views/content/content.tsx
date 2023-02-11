394159d50b67e10dac8678db62757cc7e4f1e825
Sriram Hariharan <sghsri@gmail.com>
2023-02-11 12:45:21 -0600

    created useTabMessage hook

--- src/views/content/content.tsx
+++ src/views/content/content.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { bMessenger } from 'src/shared/messages';
+import { ContextInvalidated, onContextInvalidated } from 'chrome-extension-toolkit';
 import { Button } from './components/Button/Button';
-import watchForContextInvalidation from './components/ContextInvalidated';
 
-const div = document.createElement('div');
-div.id = 'root';
+const root = document.createElement('div');
+root.id = 'root';
 
-document.body.appendChild(div);
+document.body.appendChild(root);
 
 console.log('content script');
 
@@ -15,8 +15,13 @@ bMessenger.getTabId(undefined).then(tabId => {
     console.log('tabId', tabId);
 });
 
-render(<Button />, div);
+render(<Button />, root);
 
 if (process.env.NODE_ENV === 'development') {
-    watchForContextInvalidation();
+    onContextInvalidated(() => {
+        const div = document.createElement('div');
+        div.id = 'context-invalidated-container';
+        document.body.appendChild(div);
+        render(<ContextInvalidated color='black' backgroundColor='orange' />, div);
+    });
 }
